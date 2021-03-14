c7922a4116ccd67a49291d43bcf9e56811eb6cbe
Yosef <shmuli5770@gmail.com>
2021-03-14 00:08:57 -0500

    added some (commented out) code to .js to run in console and debug

---
+++ script.js
@@ -0,0 +1,12 @@
+/*const theOfficePic = document.getElementById('mscott');
+let laughedMore = false;
+if (theOfficePic) {
+    theOfficePic.addEventListener("mouseenter", function(event) {
+        if (!laughedMore) {
+            const moreLaughs = createElement('h3');
+            moreLaughs.innerHTML = 'Haha... aha... ha... :|';
+            document.getElementById('jokeSection').appendChild(moreLaughs);
+            laughedMore = true
+        }
+    })
+}*/
