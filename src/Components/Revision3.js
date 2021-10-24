233e4ba72022b5d49acf8ca7b1ba6f55b5bf9823
Yosef Adelman <shmuli5770@gmail.com>
2021-10-24 13:18:19 -0400

    add video for 3rd revision tab

--- src/Components/Revision3.js
+++ src/Components/Revision3.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import ReactPlayer from "react-player";
 
 function Revision3(props) {
   return (
     <div>
-      Demo Postgresql & Ruby on Rails backend, React backend interface,
-      React-Native frontend
+      <Container>
+        <h2>Watch me demonstrate using the 3rd revision of RapidbuS</h2>
+        <ReactPlayer url="https://youtu.be/oh4HZoyXtHI" controls />
+      </Container>
     </div>
   );
 }
