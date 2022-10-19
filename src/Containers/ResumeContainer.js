42f23ad0a025d689a3cd9ca8f4476d92908669cd
Yosef Adelman <shmuli5770@gmail.com>
2022-10-19 13:02:55 -0400

    update react-pdf, add code to correct allignment issues

--- src/Containers/ResumeContainer.js
+++ src/Containers/ResumeContainer.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/umd/Page/AnnotationLayer.css";
 
 function ResumeContainer() {
   return (
-    <div>
-      <Document
-        file={
-          "https://docs.google.com/document/d/1Z2njzUfNuKxbuRA3sgLVUwPeiN-QvblKd8YtkimoMzU/export?format=pdf"
-        }
-      >
-        <Page pageNumber={1} />
-      </Document>
-    </div>
+    <Document
+      file={
+        "https://docs.google.com/document/d/1Z2njzUfNuKxbuRA3sgLVUwPeiN-QvblKd8YtkimoMzU/export?format=pdf"
+      }
+    >
+      <Page
+        onLoadSuccess={() => {
+          const textLayers = document.querySelectorAll(
+            ".react-pdf__Page__textContent"
+          );
+          textLayers.forEach((layer) => {
+            const { style } = layer;
+            style.top = "0";
+            style.left = "0";
+            style.transform = "";
+          });
+        }}
+        pageNumber={1}
+      />
+    </Document>
   );
 }
 
